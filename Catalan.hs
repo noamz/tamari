ccbe18a2fc54f9c75de152adb6dde3a9d6749e8a
Noam Zeilberger <noam.zeilberger@gmail.com>
2019-03-19 14:01:39 +0000

    tree -> bin

--- Catalan.hs
+++ Catalan.hs
@@ -9,84 +9,84 @@ import System.Random
 
 import Bijections
 
-data Tree = L | B Tree Tree
+data Bin = L | B Bin Bin
   deriving (Show,Eq,Ord,Typeable)
 
-foldTree :: a -> (a -> a -> a) -> Tree -> a
-foldTree l b L = l
-foldTree l b (B t1 t2) = b (foldTree l b t1) (foldTree l b t2)
+foldBin :: a -> (a -> a -> a) -> Bin -> a
+foldBin l b L = l
+foldBin l b (B t1 t2) = b (foldBin l b t1) (foldBin l b t2)
 
 -- dualization (mirror image) operation on binary trees
 
-dualtree :: Tree -> Tree
-dualtree L = L
-dualtree (B t1 t2) = B (dualtree t2) (dualtree t1)
+dualbin :: Bin -> Bin
+dualbin L = L
+dualbin (B t1 t2) = B (dualbin t2) (dualbin t1)
 
 -- graft product: lgraft t1 t2 grafts t1 onto the leftmost leaf of t2
 
-lgraft t1 t2 = foldr (\x y -> B y x) t1 (tree2spine t2)
+lgraft t1 t2 = foldr (\x y -> B y x) t1 (bin2spine t2)
 
 -- rgraft t1 t2 grafts t2 onto the rightmost leaf of t1
-rgraft t1 t2 = dualtree (lgraft (dualtree t2) (dualtree t1))
+rgraft t1 t2 = dualbin (lgraft (dualbin t2) (dualbin t1))
 
--- factoring a tree along its left-branching spine
+-- factoring a binary tree along its left-branching spine
 
-tree2spine :: Tree -> [Tree]
-tree2spine L = []
-tree2spine (B t1 t2) = t2 : tree2spine t1
+bin2spine :: Bin -> [Bin]
+bin2spine L = []
+bin2spine (B t1 t2) = t2 : bin2spine t1
 
-spine2tree :: [Tree] -> Tree
-spine2tree s = foldr (\y x -> B x y) L s
+spine2bin :: [Bin] -> Bin
+spine2bin s = foldr (\y x -> B x y) L s
 
 -- statistics
-nodes :: Tree -> Int
+nodes :: Bin -> Int
 nodes L = 0
 nodes (B t1 t2) = 1 + nodes t1 + nodes t2
 
-leaves :: Tree -> Int
+leaves :: Bin -> Int
 leaves L = 1
 leaves (B t1 t2) = leaves t1 + leaves t2
 
 -- generate all binary trees with n nodes
-binary_trees :: Int -> [Tree]
+binary_trees :: Int -> [Bin]
 binary_trees 0 = [L]
 binary_trees n = [B t1 t2 | i <- [0..n-1], t1 <- binary_trees i, t2 <- binary_trees (n-1-i)]
 
 -- procedure for sprouting a leaf off to the left or right (used in Rémy's algorithm)
-sprout :: Tree -> Bool -> Int -> Tree
+sprout :: Bin -> Bool -> Int -> Bin
 sprout t dir n = sprout_cps t dir n id (error "sprout: index out of bounds")
   where
     -- written in continuation passing style using success and failure continuations
-    sprout_cps :: Tree -> Bool -> Int -> (Tree -> r) -> (Int -> r) -> r
+    sprout_cps :: Bin -> Bool -> Int -> (Bin -> r) -> (Int -> r) -> r
     sprout_cps t dir 0 ks kf = ks (if dir then B L t else B t L)
     sprout_cps (B t1 t2) dir n ks kf = sprout_cps t1 dir (n-1) (\t1' -> ks (B t1' t2))
                                        (\n' -> sprout_cps t2 dir n' (\t2' -> ks (B t1 t2')) kf)
     sprout_cps L dir n ks kf = kf (n-1)
 
 -- generate a uniformly random binary tree with n nodes via Rémy's algorithm
-remy_tree :: RandomGen g => Int -> g -> (Tree,g)
-remy_tree 0 g = (L, g)
-remy_tree n g =
-  let (i,g') = randomR (0,2*n-2) g in
-  let (lr,g'') = randomR (False,True) g' in
-  let (t,g''') = remy_tree (n-1) g'' in
-  (sprout t lr i, g''')
+remy_bin :: RandomGen g => Int -> g -> (Bin,g)
+remy_bin 0 g = (L, g)
+remy_bin n g =
+  let (t,g') = remy_bin (n-1) g in
+  let (i,g'') = randomR (0,2*n-2) g' in
+  let (d,g''') = randomR (False,True) g'' in
+  (sprout t d i, g''')
 
-remy_tree' :: Int -> IO Tree
-remy_tree' n = getStdRandom (remy_tree n)
+remy_bin' :: Int -> IO Bin
+remy_bin' n = getStdRandom (remy_bin n)
 
 -- three different ways of converting binary trees to matching parentheses
-showTreeL :: Tree -> String
-showTreeL L = "*"
-showTreeL (B t1 t2) = "(" ++ showTreeL t1 ++ ")" ++ showTreeL t2
+showBinL :: Bin -> String
+showBinL L = "*"
+showBinL (B t1 t2) = "(" ++ showBinL t1 ++ ")" ++ showBinL t2
 
-showTreeU :: Tree -> String
-showTreeU L = "*"
-showTreeU (B t1 t2) = "(" ++ showTreeU t1 ++ showTreeU t2 ++ ")"
+showBinU :: Bin -> String
+showBinU L = "*"
+showBinU (B t1 t2) = "(" ++ showBinU t1 ++ showBinU t2 ++ ")"
 
-showTreeR :: Tree -> String
-showTreeR L = "*"
-showTreeR (B t1 t2) = showTreeR t1 ++ "(" ++ showTreeR t2 ++ ")"
+showBinR :: Bin -> String
+showBinR L = "*"
+showBinR (B t1 t2) = showBinR t1 ++ "(" ++ showBinR t2 ++ ")"
 
 -- conversion between binary trees and arc diagrams/double-occurrence words
 data Arc = U Int | D Int
@@ -98,19 +98,19 @@ isup (D _) = False
 isdown (U _) = False
 isdown (D _) = True
 
-arcs2tree_cps :: Int -> Arcs -> (String -> r) -> (Arcs -> Tree -> r) -> r
-arcs2tree_cps x (D y:w) fail k = if x == y then k w L else fail "not a planar arc diagram"
-arcs2tree_cps x (U y:w) fail k = arcs2tree_cps y w fail $ \w' t1 -> arcs2tree_cps x w' fail $ \w'' t2 -> k w'' (B t1 t2)
-arcs2tree_cps x [] fail k = fail "not a closed arc diagram"
+arcs2bin_cps :: Int -> Arcs -> (String -> r) -> (Arcs -> Bin -> r) -> r
+arcs2bin_cps x (D y:w) fail k = if x == y then k w L else fail "not a planar arc diagram"
+arcs2bin_cps x (U y:w) fail k = arcs2bin_cps y w fail $ \w' t1 -> arcs2bin_cps x w' fail $ \w'' t2 -> k w'' (B t1 t2)
+arcs2bin_cps x [] fail k = fail "not a closed arc diagram"
 
-arcs2tree :: Arcs -> Tree
-arcs2tree [] = L
-arcs2tree (U x:xs) = arcs2tree_cps x xs (\s -> error s) (\w' t -> B t (arcs2tree w'))
-arcs2tree (D x:xs) = error "not a closed arc diagram"
+arcs2bin :: Arcs -> Bin
+arcs2bin [] = L
+arcs2bin (U x:xs) = arcs2bin_cps x xs (\s -> error s) (\w' t -> B t (arcs2bin w'))
+arcs2bin (D x:xs) = error "not a closed arc diagram"
 
 isDyck :: Arcs -> Bool
 isDyck [] = True
-isDyck (U x:xs) = arcs2tree_cps x xs (\_ -> False) (\_ _ -> True)
+isDyck (U x:xs) = arcs2bin_cps x xs (\_ -> False) (\_ _ -> True)
 isDyck (D _:_) = False
 
 isClosed :: Arcs -> Bool
@@ -120,15 +120,15 @@ isClosed (U x:xs) =
   if null post then False else isClosed (pre ++ tail post)
 isClosed (D x:xs) = False
 
-tree2arcs_st :: Tree -> Int -> (Int,Arcs)
-tree2arcs_st L n = (n,[])
-tree2arcs_st (B t1 t2) n =
-  let (n',w1) = tree2arcs_st t1 (n+1) in
-  let (n'',w2) = tree2arcs_st t2 n' in
+bin2arcs_st :: Bin -> Int -> (Int,Arcs)
+bin2arcs_st L n = (n,[])
+bin2arcs_st (B t1 t2) n =
+  let (n',w1) = bin2arcs_st t1 (n+1) in
+  let (n'',w2) = bin2arcs_st t2 n' in
   (n'',U n : w1 ++ D n : w2)
 
-tree2arcs :: Tree -> Arcs
-tree2arcs t = let (n,w) = tree2arcs_st t 0 in w
+bin2arcs :: Bin -> Arcs
+bin2arcs t = let (n,w) = bin2arcs_st t 0 in w
 
 normalizeArcs :: Arcs -> Arcs
 normalizeArcs w =
@@ -180,51 +180,51 @@ arcs2inv p =
         D x -> fromJust (findIndex (== U x) p) in
   [(i,act i) | i <- [0..n-1]]
 
--- coercions checking that a tree has a special form
+-- coercions checking that a binary tree has a special form
 
-rightleaf :: Tree -> Tree
+rightleaf :: Bin -> Bin
 rightleaf (B t L) = t
-rightleaf _ = error "tree not of form (B t L)"
+rightleaf _ = error "binary tree not of form (B t L)"
 
-leftleaf :: Tree -> Tree
+leftleaf :: Bin -> Bin
 leftleaf (B L t) = t
-leftleaf _ = error "tree not of form (B L t)"
+leftleaf _ = error "binary tree not of form (B L t)"
 
 
-type TreePath =  [Either () ()]
+type BinPath =  [Either () ()]
   
-paths :: Tree -> [TreePath]
+paths :: Bin -> [BinPath]
 paths L = [[]]
 paths (B t1 t2) = [Left () : p1 | p1 <- paths t1] ++ [Right () : p2 | p2 <- paths t2]
 
-ldepth :: TreePath -> Int
+ldepth :: BinPath -> Int
 ldepth (Left ():p) = 1 + ldepth p
 ldepth _ = 0
 
-rdepth :: TreePath -> Int
+rdepth :: BinPath -> Int
 rdepth (Right ():p) = 1 + rdepth p
 rdepth _ = 0
 
 -- evaluate a binary tree in left-to-right order, given interpretations for the leaves and
 -- interpretations for the internal nodes as binary operations
-evalLR :: Tree -> ([a],[a -> a -> a]) -> ([a],[a -> a -> a])
+evalLR :: Bin -> ([a],[a -> a -> a]) -> ([a],[a -> a -> a])
 evalLR L (stk,ctl) = (stk,ctl)
 evalLR (B t1 t2) (stk,f:ctl) =
   let (x:stk',ctl') = evalLR t1 (stk,ctl) in
   let (y:stk'',ctl'') = evalLR t2 (stk',ctl') in
   (f x y:stk'',ctl'')
 
--- for a given tree, returns all trees which match it from the root
-matchroot :: Tree -> [Tree]
+-- for a given binary tree, returns all binary trees which match it from the root
+matchroot :: Bin -> [Bin]
 matchroot L = [L]
 matchroot (B t1 t2) = L : [B u1 u2 | u1 <- matchroot t1, u2 <- matchroot t2]
 
--- for a given tree, returns all occurrences of subtrees
-subtrees :: Tree -> [Tree]
+-- for a given binary tree, returns all occurrences of subtrees
+subtrees :: Bin -> [Bin]
 subtrees L = [L]
 subtrees (B t1 t2) = subtrees t1 ++ subtrees t2  ++ matchroot (B t1 t2)
 
-patternseries :: Tree -> [[Int]]
+patternseries :: Bin -> [[Int]]
 patternseries p =
   let n = nodes p in
   [[length $ filter (==i) [length $ filter (==p) (subtrees t) | t <- binary_trees m] | i <- [0..m]] | m <- [0..]]
--- Tamari.hs
+++ Tamari.hs
@@ -8,28 +8,28 @@ import Data.Maybe
 import Catalan
 import Control.Monad
 
-rotR1 :: Tree -> [Tree]
+rotR1 :: Bin -> [Bin]
 rotR1 (B (t1 @ (B t11 t12)) t2) =
   B t11 (B t12 t2) : [B t1' t2 | t1' <- rotR1 t1] ++ [B t1 t2' | t2' <- rotR1 t2]
 rotR1 (B L t2) = [B L t2' | t2' <- rotR1 t2]
 rotR1 _ = []
 
-rotL1 :: Tree -> [Tree]
+rotL1 :: Bin -> [Bin]
 rotL1 (B t1 (t2 @ (B t21 t22))) =
   B (B t1 t21) t22 : [B t1' t2 | t1' <- rotL1 t1] ++ [B t1 t2' | t2' <- rotL1 t2]
 rotL1 (B t1 L) = [B t1' L | t1' <- rotL1 t1]
 rotL1 _ = []
 
-tamari_up :: Tree -> [Tree]
+tamari_up :: Bin -> [Bin]
 tamari_up t = t : foldr union [] [tamari_up t' | t' <- rotR1 t]
 
-tamari_down :: Tree -> [Tree]
+tamari_down :: Bin -> [Bin]
 tamari_down t = t : foldr union [] [tamari_down t' | t' <- rotL1 t]
 
-tamari_order :: Tree -> Tree -> Bool
+tamari_order :: Bin -> Bin -> Bool
 tamari_order t1 t2 = elem t2 (tamari_up t1)
 
-kreweras_order :: Tree -> Tree -> Bool
+kreweras_order :: Bin -> Bin -> Bool
 kreweras_order L L = True
 kreweras_order (B t1 t2) (B t1' t2') =
   (kreweras_order t1 t1' && kreweras_order t2 t2') ||
@@ -38,11 +38,11 @@ kreweras_order (B t1 t2) (B t1' t2') =
     L -> False
 kreweras_order _ _ = False
 
-tamari :: Int -> [(Tree,Tree)]
+tamari :: Int -> [(Bin,Bin)]
 tamari n = [(t1,t2) | t1 <- binary_trees n, t2 <- tamari_up t1]
 -- [length $ tamari n | n <- [0..]] == [1,1,3,13,68,399,2530,...]
 
-kreweras :: Int -> [(Tree,Tree)]
+kreweras :: Int -> [(Bin,Bin)]
 kreweras n = [(t1,t2) | t1 <- binary_trees n, t2 <- binary_trees n, kreweras_order t1 t2]
 
 tamari_parts :: Int -> [Int]
@@ -55,10 +55,10 @@ tamari_parts n = [length $ tamari_down t | t <- binary_trees n]
 prop1 :: Int -> Bool
 prop1 n =
   flip all (tamari n) $ \(t1,t2) ->
-  length (tree2spine t1) >= length (tree2spine t2)
+  length (bin2spine t1) >= length (bin2spine t2)
 
 -- sequent-style decision procedure for Tamari order
-tamari_seq :: [Tree] -> Tree -> Tree -> Bool
+tamari_seq :: [Bin] -> Bin -> Bin -> Bool
 tamari_seq g (B t1 t2) u = tamari_seq (t2:g) t1 u
 tamari_seq g L L = g == []
 tamari_seq g L (B u1 u2) =
@@ -84,9 +84,9 @@ prop2 n =
   tamari_order t1 t2 == tamari_seq [] t1 t2
 
 -- focused sequent calculus
-tamari_linv :: Tree -> [Tree] -> Tree -> Bool
-tamari_neu :: [Tree] -> Tree -> Bool
-tamari_linv t g u = let ts = tree2spine t in tamari_neu (reverse ts ++ g) u
+tamari_linv :: Bin -> [Bin] -> Bin -> Bool
+tamari_neu :: [Bin] -> Bin -> Bool
+tamari_linv t g u = let ts = bin2spine t in tamari_neu (reverse ts ++ g) u
 tamari_neu g L = g == []
 tamari_neu g (B u1 u2) =
   let k = leaves u1 in
@@ -104,7 +104,7 @@ tamari_neu g (B u1 u2) =
     Just (g1,[]) -> False
 
 -- faster generation of intervals
-tamari' :: Int -> [(Tree,Tree)]
+tamari' :: Int -> [(Bin,Bin)]
 tamari' n = [(t1,t2) | t1 <- binary_trees n, t2 <- binary_trees n, tamari_linv t1 [] t2]
 
 -- soundness and completeness of the focused sequent calculus
@@ -115,27 +115,27 @@ prop3 n =
   tamari_linv t1 [] t2 == tamari_seq [] t1 t2
 
 -- lattice structure
-max_decomp :: Tree -> [Tree] -> [Tree]
+max_decomp :: Bin -> [Bin] -> [Bin]
 max_decomp L acc = L : acc
 max_decomp (B t1 t2) acc = max_decomp t1 (t2 : acc)
 
-max_recomp :: [Tree] -> Tree
+max_recomp :: [Bin] -> Bin
 max_recomp (t:ts) = foldl B t ts
 
-tamari_bot :: Int -> Tree
+tamari_bot :: Int -> Bin
 tamari_bot n = iterate (\x -> B x L) L !! n
 
-tamari_top :: Int -> Tree
+tamari_top :: Int -> Bin
 tamari_top n = iterate (\x -> B L x) L !! n
 
-tamari_join :: Tree -> Tree -> Tree
+tamari_join :: Bin -> Bin -> Bin
 tamari_join L L = L
 tamari_join t1 t2 =
   let (L:g1) = max_decomp t1 [] in
   let (L:g2) = max_decomp t2 [] in
   max_recomp (L:tamari_seqjoin g1 g2)
 
-tamari_seqjoin :: [Tree] -> [Tree] -> [Tree]
+tamari_seqjoin :: [Bin] -> [Bin] -> [Bin]
 tamari_seqjoin [] [] = []
 tamari_seqjoin [] (a2:g2) = error "tamari_seqjoin : |g1| < |g2|"
 tamari_seqjoin (a1:g1) [] = error "tamari_seqjoin : |g1| > |g2|"
@@ -148,13 +148,13 @@ tamari_seqjoin (a1:g1) (a2:g2) =
     tamari_seqjoin (a1:g1) (B a2 (head g2) : (tail g2))
   else tamari_join a1 a2 : tamari_seqjoin g1 g2
 
-tamari_meet :: Tree -> Tree -> Tree
+tamari_meet :: Bin -> Bin -> Bin
 tamari_meet t1 t2 =
   let n = nodes t1 in
   foldr tamari_join (tamari_bot n) [t | t <- binary_trees n, tamari_linv t [] t1, tamari_linv t [] t2]
 
-tamari_meet' :: Tree -> Tree -> Tree
-tamari_meet' t1 t2 = dualtree (tamari_join (dualtree t1) (dualtree t2))
+tamari_meet' :: Bin -> Bin -> Bin
+tamari_meet' t1 t2 = dualbin (tamari_join (dualbin t1) (dualbin t2))
 
 prop4 :: Int -> Bool
 prop4 n =
@@ -174,19 +174,19 @@ prop6 n =
   flip all (binary_trees n) $ \t2 ->
   tamari_linv t1 [] t2 == (tamari_meet' t1 t2 == t1)
 
-tree_type :: Tree -> [Bool]
-tree_type t = pol False t
+bin_type :: Bin -> [Bool]
+bin_type t = pol False t
   where
-    pol :: Bool -> Tree -> [Bool]
+    pol :: Bool -> Bin -> [Bool]
     pol b L = [b]
     pol b (B t1 t2) = pol False t1 ++ pol True t2
 
 -- canopy intervals
-ntamari_linv :: Tree -> [Tree] -> Tree -> Bool
-ntamari_neu :: [Tree] -> Tree -> Bool
+ntamari_linv :: Bin -> [Bin] -> Bin -> Bool
+ntamari_neu :: [Bin] -> Bin -> Bool
 ntamari_linv L [] L = True
 ntamari_linv L g _ = False
-ntamari_linv t g u = let ts = tree2spine t in ntamari_neu (reverse ts ++ g) u
+ntamari_linv t g u = let ts = bin2spine t in ntamari_neu (reverse ts ++ g) u
 ntamari_neu g L = g == []
 ntamari_neu g (B u1 u2) =
   let k = leaves u1 in
@@ -218,8 +218,8 @@ tamintprob n samples = do
   where
     experiment :: IO Bool
     experiment = do
-      t1 <- remy_tree' n
-      t2 <- remy_tree' n
+      t1 <- remy_bin' n
+      t2 <- remy_bin' n
       return $ tamari_linv t1 [] t2
 {-
 > tamintprob 5 1000000
@@ -228,4 +228,7 @@ tamintprob n samples = do
 > tamintprob 12 1000000
 8515
 -- compare to expected probability = 82861/9598268 = 0.00863291...
+> tamintprob 20 100000
+19
+-- compare to expected probability = 82300857/491762021465 = 0.000167359...
 -}
